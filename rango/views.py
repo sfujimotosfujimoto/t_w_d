c3169dbee5e57120a093ef1938c9d6063cfad840
S Fujimoto <unicoshun@gmail.com>
2017-01-22 23:55:16 +0900

    6 end

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from rango.models import Category, Page
+
+
+
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-    return render(request, 'rango/index.html', context=context_dict)
-    # return HttpResponse("Rango says hey there partner!<br/><a href='/rango/about/'>About</a>")
+    category_list = Category.objects.order_by('-views')[:5]
+    pages_list = Page.objects.order_by('-views')[:5]
+    context_dict = {
+        'categories': category_list,
+        'pages': pages_list
+    }
+
+    return render(request, 'rango/index.html', context_dict)
+
 
 def about(request):
     return HttpResponse("Rango says here is the about page!<br/><a href='/rango/'>Index</a>")
+
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)
